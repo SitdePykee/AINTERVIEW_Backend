793c871d43eba75753acdfdee22f00153eb65861
SitdePykee <darkgamers751@gmail.com>
2025-10-11 22:51:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from blueprints.authentication import auth_bp
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return "Backend is running"
+
+
     app.register_blueprint(question_bp, url_prefix="/question")
     app.register_blueprint(interview_bp, url_prefix="/interview")
     app.register_blueprint(syllabus_bp, url_prefix="/syllabus")
@@ -15,4 +21,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=True)
